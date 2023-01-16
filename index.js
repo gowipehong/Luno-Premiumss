b5828d62c530fcc3bc83ec5a2c273de53269fb82
Hong <gowipehong@gmail.com>
2023-01-16 11:00:51 +0800

    Implemented binance and price diff

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require("dotenv").config()
+const Binance = require('node-binance-api');
+
 async function lunoPrice() {
     const res = await fetch("https://api.luno.com/api/1/tickers")
     const ok = await res.json()
@@ -25,3 +27,37 @@ async function forexCurrency() {
     })
 }
 
+async function binanceCoin() {
+    const binance = new Binance()
+    let ticker = await binance.prices()
+    let btc = ticker.BTCUSDT
+    return btc
+}
+
+async function conversion() {
+    return await lunoPrice() / await forexCurrency()
+}
+
+async function priceDiff() {
+    return await conversion() - await binanceCoin()
+}
+
+async function premiumRate() {
+    return (await priceDiff() / await conversion()) * 100
+}
+
+async function listOfAllPrice() {
+    const btcLunoPrice = await lunoPrice()
+    const exchangeRate = await forexCurrency()
+    const btcBinancePrice = await binanceCoin()
+    const usdToMyr = await conversion()
+    const priceDiffBtc = await priceDiff()
+    const premiumDiff = await premiumRate()
+    console.log("BTCMYR price on LUNO:".padEnd(30, ' ') + `MYR ${btcLunoPrice}`)
+    console.log("USDMYR:".padEnd(30, ' ') + `${exchangeRate}`)
+    console.log("BTCBUSD price on Binance:".padEnd(30, ' ') + `USD ${btcBinancePrice}`)
+    console.log("BTCUSD price on LUNO:".padEnd(30, ' ') + `USD ${usdToMyr}`)
+    console.log("LUNO Premium:".padEnd(30, ' ') + `${Math.round(premiumDiff * 10000) / 10000}%`)
+}
+
+listOfAllPrice()
